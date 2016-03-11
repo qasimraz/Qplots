3c518efb916795c703341468b3e956dc814ff080
Qasim <qasimraza788@gmail.com>
2016-03-11 17:16:07 -0500

    Added README

---
+++ data_ext/mocsic.py
@@ -0,0 +1,90 @@
+import sys,csv,os
+
+'''Strings'''
+li='''File_name
+Time
+Max. Battery Voltage (V)
+Min. Battery Voltage (V)
+Charge Amp Hours (Ah)
+Load Amp Hours (A)
+Max Load Current (A)
+Max. PV Current (A)
+Max. PV Voltage (V)
+Min. PV Voltage (V)
+Batt State of Charge (SOC%)
+Max Battery Temp
+Min Battery Temp
+Length of Night (h)
+ALC
+Flags'''.split("\n")
+
+months='''January 
+February 
+March 
+April 
+May 
+June 
+July 
+August 
+September 
+October 
+November 
+December'''.split("\n")
+
+# get files
+files=os.listdir('.')
+files=[i for i in files if i.endswith('.cisdl')]
+'''Day file'''
+f=open("day2.csv", 'w')
+writer = csv.writer(f)
+writer.writerow(li)
+'''Month file'''
+d=open("month2.csv", 'w')
+month = csv.writer(d)
+month.writerow(li)
+
+for i in files:
+    c=0
+    with open(i) as fil:
+        '''Process Days 1 - 30'''
+        for j in fil.readlines()[18:72]: #18-48 for days
+            c+=1
+            byt_day=list(map(int,j.split(";")[:-1]))
+            umax = (9.0 + byt_day[0] * 0.1) # Max Battery Voltage
+            umin = (9.0 + byt_day[1] * 0.1) # Min Battery Voltage
+            CAh=2.56*byt_day[3]+byt_day[2]/100.0 # Charge Amp Hours
+            LAh=2.56*byt_day[5]+byt_day[4]/100.0 # Load Amp Hours
+            umaxmodul=byt_day[6]/2.0 #max pv voltage
+            uminmodul=byt_day[7]/2.0 #min pv voltage
+            imaxload=byt_day[8]/2.0 #Max load current
+            imaxpv=byt_day[9]/2.0 #PV current
+            SOC=int(6.66*(byt_day[10]%16)+.5)
+            ALC=int(0.4166*(byt_day[10]-byt_day[10]%16)+.5)
+            TMax=[byt_day[11],byt_day[11]-256][byt_day[11]>128]
+            TMin = [byt_day[12],byt_day[12]-256][byt_day[12]>128]
+            night=byt_day[13]/6.0
+            flags=str(bin(byt_day[14])).lstrip('0')
+            if night==42.5: # check if data is valid
+                continue
+            pr=[i,c,umax,umin,CAh,LAh,imaxload,imaxpv,umaxmodul,uminmodul,SOC,TMax,TMin,night,ALC,flags]
+            #[print(li[i],pr[i]) for i in range(len(li))]
+            if c<31: writer.writerow(pr)
+            else:
+                pr[1]-=30
+                month.writerow(pr)
+            
+        c=0
+'''Post - Processing of csv'''
+f.close()
+d.close()
+
+content = open("day2.csv", "r").read().replace('\r\n','\n')
+with open("day.csv", "w") as g:
+    g.write(content)
+os.remove('day2.csv')
+
+content = open("month2.csv", "r").read().replace('\r\n','\n')
+with open("month.csv", "w") as g:
+    g.write(content)
+os.remove('month2.csv')
+
--- qplot/c.py
+++ qplot/c.py
@@ -4,24 +4,65 @@ import matplotlib.pyplot as plt
 # import func
 
 df = pd.read_csv('day.csv')  # take data from csv make table
+weather = pd.read_csv('weather.csv')
 
+# The Max Weather Plot
+time_average1 = []
+weather_data1 = []
+weather_data2 = []
+for i in range(1,31):
+    time_average1.append(np.mean(df[df['Time'] == i].iloc[:, 11]))
+    weather_data2.append(float(weather[weather['Time'] == i].iloc[:, 3]))
+    weather_data1.append(float(weather[weather['Time'] == i].iloc[:, 2]))
+plt.plot(xrange(1,31), df[df['Name'] == 15].iloc[:, 11], label='Max Temp Pole:15', color='orange', alpha=0.7)
+plt.plot(xrange(1,31), weather_data1, label='Weather Temp High', color='red')
+plt.plot(xrange(1,31), weather_data2, label='Weather Temp Avg', color='green')
+plt.plot(xrange(1,31), time_average1, label='Average Max Temp', color='yellow')
+plt.ylabel('Temperature')
+plt.xlabel('Time (Day)')
+plt.title('Average Temperature Max')
+plt.legend(loc='upper right', shadow=True)
+plt.show()
+
+# Min Weather Plot
+time_average1 = []
+weather_data1 = []
+weather_data2 = []
+for i in range(1,31):
+    time_average1.append(np.mean(df[df['Time'] == i].iloc[:, 12]))
+    weather_data2.append(float(weather[weather['Time'] == i].iloc[:, 3]))
+    weather_data1.append(float(weather[weather['Time'] == i].iloc[:, 4]))
+plt.plot(xrange(1,31), df[df['Name'] == 15].iloc[:, 13], label='Min Temp Pole:15', color='orange', alpha=0.7)
+plt.plot(xrange(1,31), weather_data1, label='Weather Temp Low', color='red')
+plt.plot(xrange(1,31), weather_data2, label='Weather Temp Avg', color='green')
+plt.plot(xrange(1,31), time_average1, label='Average Min Temp', color='yellow')
+plt.ylabel('Temperature')
+plt.xlabel('Time (Day)')
+plt.title('Average Temperature Min')
+plt.legend(loc='lower right', shadow=True)
+plt.show()
+
+pole_total1 = []
+pole_total2 = []
+for i in list(np.unique(df.iloc[:, 0])):
+    pole_total1.append(sum(df[df['Name'] == i].iloc[:, 7]))
+    pole_total2.append(np.std(df[df['Name'] == i].iloc[:, 7]))
+plt.bar(list(np.unique(df.iloc[:, 0])), pole_total1, align='center')
+plt.xticks(list(np.unique(df.iloc[:, 0])))
+plt.ylabel('Cumulative Max Current (A) from PV')
+plt.xlabel('Pole Number')
+plt.title('Current per Pole')
+plt.show()
+'''
 time_average1 = []
 time_average2 = []
-for i in xrange(30):
+for i in list(np.unique(df.iloc[:, 0])):
     time_average1.append(np.mean(df[df['Time'] == i+1].iloc[:, 12]))
     time_average2.append(np.mean(df[df['Time'] == i+1].iloc[:, 13]))
 plt.plot(xrange(30), time_average1)
 plt.plot(xrange(30), time_average2)
-plt.ylabel('Average PV Current (A)')
+plt.ylabel('Temperature')
 plt.xlabel('Time (Day)')
-plt.title('Solar Energy vs Time')
+plt.title('Average Temperature Max/Min')
 plt.show()
-
-pole_total = []
-for i in list(np.unique(df.iloc[:, 0])):
-    pole_total.append(sum(df[df['Name'] == i].iloc[:, 7]))
-plt.bar(np.unique(df.iloc[:, 0]), pole_total)
-plt.ylabel('Cumulative total current (A) from PV')
-plt.xlabel('Pole Number')
-plt.title('Solar Energy vs Poles')
-plt.show()
+'''
