c92208db16ec8c2973ba62a828049ad94841ef99
Qasim <qasimraza788@gmail.com>
2016-03-11 14:27:01 -0500

    Added README

--- qplot/c.py
+++ qplot/c.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-#import func
+# import func
 
-df = pd.read_csv('day.csv') # take data from csv make table
+df = pd.read_csv('day.csv')  # take data from csv make table
 
 time_average1 = []
 time_average2 = []
@@ -18,7 +18,7 @@ plt.title('Solar Energy vs Time')
 plt.show()
 
 pole_total = []
-for i in np.unique(df.iloc[:, 0]):
+for i in list(np.unique(df.iloc[:, 0])):
     pole_total.append(sum(df[df['Name'] == i].iloc[:, 7]))
 plt.bar(np.unique(df.iloc[:, 0]), pole_total)
 plt.ylabel('Cumulative total current (A) from PV')
