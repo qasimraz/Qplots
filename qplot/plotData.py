7c31add754dd301ca99e8a4aabbc7c110fb9496d
Qasim <qasimraza788@gmail.com>
2016-03-21 18:11:46 -0400

    Reorganize

--- qplot/plotData.py
+++ qplot/plotData.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-# import func
 
 df = pd.read_csv('data_day_2.27.2016.csv')  # take data from csv make table
 weather = pd.read_csv('data_weather.csv')
