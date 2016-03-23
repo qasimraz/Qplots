b0c7df15db9d315be864f64af112789bdbe869f8
Qasim <qasimraza788@gmail.com>
2016-03-23 15:51:54 -0400

    Reorganize

--- qplot/plotData.py
+++ qplot/plotData.py
@@ -2,7 +2,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('data_day_2.27.2016.csv')  # take data from csv make table
+#df = pd.read_csv('data_day_2.27.2016.csv')  # take data from csv make table
+df = pd.read_csv('3.19.2016/data_day_3.19.2016.csv')
 weather = pd.read_csv('data_weather.csv')
 
 # The Max Weather Plot
@@ -42,27 +43,34 @@ plt.title('Average Temperature Min')
 plt.legend(loc='lower right', shadow=True)
 plt.show()
 
+# PV Current per pole
 pole_total1 = []
 pole_total2 = []
 for i in list(np.unique(df.iloc[:, 0])):
-    pole_total1.append(sum(df[df['Name'] == i].iloc[:, 7]))
-    pole_total2.append(np.std(df[df['Name'] == i].iloc[:, 7]))
+    pole_total1.append(sum(df[df['File_name'] == i].iloc[:, 7]))
+    pole_total2.append(np.std(df[df['File_name'] == i].iloc[:, 7]))
 plt.bar(list(np.unique(df.iloc[:, 0])), pole_total1, align='center')
 plt.xticks(list(np.unique(df.iloc[:, 0])))
 plt.ylabel('Cumulative Max Current (A) from PV')
 plt.xlabel('Pole Number')
 plt.title('Current per Pole')
 plt.show()
-'''
-time_average1 = []
-time_average2 = []
+
+# Flags raised by pole
+flags_total = [][]
 for i in list(np.unique(df.iloc[:, 0])):
-    time_average1.append(np.mean(df[df['Time'] == i+1].iloc[:, 12]))
-    time_average2.append(np.mean(df[df['Time'] == i+1].iloc[:, 13]))
-plt.plot(xrange(30), time_average1)
-plt.plot(xrange(30), time_average2)
-plt.ylabel('Temperature')
-plt.xlabel('Time (Day)')
-plt.title('Average Temperature Max/Min')
+    for j in list(np.unique(df.iloc[:, 0])):
+        flags_total[i].append(count(df[df['Flags'] == j].iloc[:, 15]))
+plt.hist(list(np.unique(df.iloc[:, 0])), df.Flags)
+plt.xticks(list(np.unique(df.iloc[:, 0])))
+plt.ylabel('Cumulative Max Current (A) from PV')
+plt.xlabel('Pole Number')
+plt.title('Current per Pole')
 plt.show()
+'''
+b11 - Low Voltage Disconnect
+b1001 - Low Voltage Disconnect + Load Overcurrent Disconnect
+b1011 - Low Voltage Disconnect + Load Overcurrent Disconnect + Full Battery
+b10011 - Full battery + Low Voltage Disconnect + Battery Emergency High Voltage
+b11011 - Full battery + Low Voltage Disconnect + Battery Emergency High Voltage + Load Overcurrent Disconnect
 '''
